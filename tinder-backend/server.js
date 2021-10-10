b8eb0d9374f2ed9c4bd34a8788801efedcb65126
subhashishshah21 <subhashishshahprofessional@gmail.com>
2021-10-10 16:02:52 +0545

    Tinder clone

---
+++ tinder-backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
+
+
+const app = express();
+const port = process.env.PORT || 8001; 
+const connection_url = "mongodb+srv://admin:B1jwJnlHCFAnRdDV@cluster0.kfiul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+app.use(express.json());
+app.use(Cors());
+
+mongoose.connect(connection_url);
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("HELLO WORLD!!!");
+  
+});
+
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  
+
+  Cards.create(dbCard, (err, data) => {
+
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      // 200 means found
+      res.status(200).send(data);
+    }
+  });
+});
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
